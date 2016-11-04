6393d63d428ad52275294f921b801bf42cbecde8
Brian Scaramella <bvs7@cornell.edu>
2016-11-03 20:13:40 -0400

    MState implementation, GroupyCommTest is currently active

--- GroupyComm.py
+++ GroupyComm.py
@@ -1,4 +1,5 @@
 # Groupy interface module
+from MInfo import * # loads in globals
 """
 This module handles all the communication with Groupme
 
@@ -6,12 +7,10 @@ you can cast to different groups
 
 you can DM to different players
 
-"""
+
 
 import groupy
 import groupy.api.endpoint as groupyEP
-
-from MInfo import * # loads in globals
 
 class NoGroupError(Exception):
   def __init__(self,msg):
@@ -24,9 +23,6 @@ class NoBotError(Exception):
 class GroupyComm:
 
   def __init__(self):
-
-    log("init")
-
     # Setup Groups
     try:
       self.mainGroup  = self.getGroup(MAIN_GROUP_ID)
@@ -43,8 +39,6 @@ class GroupyComm:
     except Exception as e:
       log("FATAL: Failed loading Groups: {}".format(e))
       exit()
-
-    log("passed groups")
 
     # Setup Direct Messages
     try:
@@ -77,16 +71,91 @@ class GroupyComm:
       log("No bot found with id {}".format(bot_id))
       raise NoBotError("No bot found with id {}".format(bot_id))
 
+  def cast(self,message,group=self.mainGroup):
+    try:
+      if not SILENT:
+        groupyEP.Messages.create(group.group_id,message)
+      log("CAST-{}: {}".format(group.name,message))
+      return True
+    except Exception as e:
+      log("FAILED TO CAST-{} {}: {}".format(group.name,message,e))
+      return False
+    
+  def sendDM(self,message,mem_id):
+    try:
+      if not SILENT:
+        groupyEP.DirectMessages.create(mem_id,message)
+      log("DM-{}: {}".format(self.getName(mem_id),message))
+      return True
+    except Exception as e:
+      log("FAILED TO DM-{} {}: {}".format(self.getName(mem_id),message,e))
+      return False
 
   def getDMs(self, player_id):
-    """Return a list of the most recent messages from a player"""
-    if player_id in self.recent_ids:
-      DMs = groupyEP.DirectMessages.index(player_id,
-                                          since_id=self.recent_ids[player_id])
-      self.recent_ids[player_id] = DMs[0]['id']
-    else:
-      DMs = groupyEP.DirectMessages.index(player_id)
-      self.recent_ids[player_id] = DMs[0]['id']
-      DMs = []
-    return DMs
-    
+    try:
+      if player_id in self.recent_ids:
+        DMs = groupyEP.DirectMessages.index(player_id,
+                                            since_id=self.recent_ids[player_id])
+        self.recent_ids[player_id] = DMs[0]['id']
+      else:
+        DMs = groupyEP.DirectMessages.index(player_id)
+        self.recent_ids[player_id] = DMs[0]['id']
+        DMs = []
+      return DMs
+    except groupyEP.APIError as e:
+      log("Failed to get DM: {}".format(e), 3) # This happens a lot, so it's silenced
+
+  def getName(self,player_id):
+    self.mainGroup.refresh()
+    members = self.mainGroup.members()
+    for m in members:
+      if m.user_id == player_id:
+        return m.nickname
+    return "__"
+
+  def addMafia(self, player_id):
+    self.mafiaGroup.add({'user_id':player_id})
+
+  def clearMafia(self):
+    # Remove all from Mafia Group
+    self.mafiaGroup.refresh()
+    for mem in self.mafiaGroup.members():
+      if not mem.user_id == self.MODERATOR:
+        self.mafiaGroup.remove(mem)
+        log("removing {} from mafia chat".format(mem.nickname))
+"""
+
+class GroupyCommTest:
+
+  def __init__(self):
+    self.mainGroup = 1
+    self.mafiaGroup = 2
+
+    self.mafia = []
+
+  def cast(self,msg,group=1):
+    m = "CAST: "
+    if group == self.mainGroup:
+      m += "(MAIN) "
+    elif group == self.mafiaGroup:
+      m += "(MAFIA) "
+    m += msg
+
+    log(m,1)
+
+  def sendDM(self,msg,player_id):
+    return "DM: "+"("+player_id+") "+msg
+
+  def getDMs(self,player_id):
+    return []
+
+  def getName(self,player_id):
+    return "[Name of {}]".format(player_id)
+
+  def addMafia(self, player_id):
+    self.mafia.append(player_id)
+
+  def clearMafia(self):
+    self.mafia.clear()
+
+  
--- MInfo.py
+++ MInfo.py
@@ -3,6 +3,7 @@
 import json  # For loading vars
 
 DEBUG = 2
+SILENT = False
 
 info_fname = "info"
 
@@ -43,3 +44,56 @@ def saveNote(var,name):
   except Exception as e:
     log("Failed to save {}: {}".format(name, e))
     raise NoteError(str(e)+": "+name)
+
+
+### DATA FOR MSTATE #############
+
+MAFIA_ROLES = [ "MAFIA" ]
+TOWN_ROLES  = [ "TOWN", "COP", "DOCTOR", "IDIOT" ]
+
+ROLE_EXPLAIN= {
+  "MAFIA" : ("You are MAFIA. You are now part of the mafia chat to talk "
+             "privately with your co-conspirators. During the day, try not "
+             "to get killed. During the Night, choose somebody to kill!"),
+  "TOWN"  : ("You are TOWN. You are a normal player in this game, the last "
+             "line of defense against the mafia scum. Sniff out who the "
+             "mafia are and convince your fellow town members to kill them "
+             "during the day!"),
+  "COP"   : ("You are a COP. You are one of the most powerful members of "
+             "the townspeople. During the night, send a direct message to me "
+             "with the number of the person you want to investigate, and "
+             "upon morning, I will tell you whether that person is town or "
+             "mafia."),
+  "DOCTOR": ("You are a DOCTOR. Your job is to save the townspeople from "
+             "the mafia scum. During the night, send a direct message to me"
+             " with the number of the person you want to save. If the mafia"
+             " targets them, they will have a near death experience, but "
+             "survive."),
+  "IDIOT" : ("You are the villiage IDIOT. Your life's dream is to be such an"
+             " annoyance that the townsfolk kill you in frustration. You don't"
+             " care whether the mafia win or lose, as long as you get votes."),
+  }
+
+# ROLE GENERATION
+
+BASE_SCORE = -8
+
+ROLE_SCORES = {
+  "MAFIA"  : -3,
+  "DOCTOR" :  4,
+  "COP"    :  4,
+  "TOWN"   :  2,
+  "IDIOT"  : -2,
+}
+
+# Probability of town roles being chosen
+TOWN_WEIGHTS = [
+  ["TOWN", "DOCTOR", "COP"],
+  [75,     10,       10]
+]
+
+# Probability of anti-town roles being chosen
+MAFIA_WEIGHTS = [
+  ["MAFIA", "IDIOT"],
+  [85,      20],
+]
